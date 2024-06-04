6cffe950fc4d03b343b10042537a36b88a126661
costigator <costigator@gmail.com>
2024-06-04 14:00:00 +0000

    chat initial commit

---
+++ chat.py
@@ -0,0 +1,8 @@
+from ctransformers import AutoModelForCausalLM
+
+llm = AutoModelForCausalLM.from_pretrained(
+    "zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf"
+)
+
+
+print(llm("Where is the capital of India? Please only the answer."))
